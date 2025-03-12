489d47ad3008b78ad8268799782073bae5e3fa28
Stamate Teodor <stamateteodor23@gmail.com>
2025-03-12 18:54:49 +0200

    add credentials

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,5 +1,24 @@
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 import GitHub from "next-auth/providers/github";
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [GitHub],
+  providers: [
+    GitHub,
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        const email = "admin@admin.com";
+        const password = "parola12";
+
+        if (credentials.email === email && credentials.password === password) {
+          return { email, password };
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
 });
